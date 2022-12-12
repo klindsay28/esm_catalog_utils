25af689b4df6c2029823b22b85360820a4450bb6
Keith Lindsay <klindsay@ucar.edu>
2022-12-12 11:29:59 -0700

    avoid long line in setup.py, add python 3.11 to ci testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     maintainer="Keith Lindsay",
     maintainer_email="klindsay@ucar.edu",
-    description="tools/utilities to support the usage of catalogs to access and analyze ESM output",
+    description="utilities to support the usage of catalogs to access ESM output",
     name="esm_catalog_utils",
     packages=find_packages(),
     url="https://github.com/klindsay28/esm_catalog_utils",
